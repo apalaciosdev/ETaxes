d0bc7b9abbb1c6df6c13f7cee0ca10ea79043234
Aaron Palacios <7613506-apalacios-dev@users.noreply.gitlab.com>
2023-04-09 11:28:42 +0200

    refact sales controller

--- backend/controllers/sales.js
+++ backend/controllers/sales.js
@@ -4,59 +4,50 @@ const Offer = require('../models/offers')
 
 
 const salesGet = async(req, res = response) => {
-
   const sales = await Sale.find();
   res.json(sales);
 }
 
 const infoSalesGet = async(req, res = response) => {
   const { mail } = req.body
-
   let salesArray = [];
-
-
-
   const productsArray = await Product.find({ user: mail })
-  
-  
-
-
   const salesCount = await Sale.countDocuments({ sellerMail: mail });
   const countUserProducts = await Product.countDocuments({ user: mail });
 
-  // const bestSelling = await Product.countDocuments({ user: mail });
+
   await Sale.find({sellerMail: mail})
   .then(sales => {
     salesArray = sales;
   })
 
 
-
+  //----------------salesGraphic----------------
   const compras = await Sale.find({sellerMail: mail})
   // Utilizamos reduce() para agrupar la información por meses
-  // Utilizamos reduce() para agrupar la información por meses
   const comprasPorMes = compras.reduce((acumulador, compra) => {
-  // Obtenemos el mes de la fecha_compra
-  const month = compra.purchaseDate.toLocaleString('es-ES', { month: 'long' });
-  
-  // Si el month no existe en el array acumulador, lo creamos
-  if (!acumulador[month]) {
-    acumulador[month] = { month: month, total: 0 };
-  }
-  
-  // Sumamos el precio de la compra al total del month correspondiente
-  acumulador[month].total += compra.price * compra.units;
-  
-  // Retornamos el acumulador
-  return acumulador;
-}, {});
+    // Obtenemos el mes de la fecha_compra
+    const month = compra.purchaseDate.toLocaleString('es-ES', { month: 'long' });
+    
+    // Si el month no existe en el array acumulador, lo creamos
+    if (!acumulador[month]) {
+      acumulador[month] = { month: month, total: 0 };
+    }
+    
+    // Sumamos el precio de la compra al total del month correspondiente
+    acumulador[month].total += compra.price * compra.units;
+    
+    // Retornamos el acumulador
+    return acumulador;
+  }, {});
 
-// Convertimos el objeto en un array
-const comprasPorMesArray = Object.values(comprasPorMes);
+  // Convertimos el objeto en un array
+  const comprasPorMesArray = Object.values(comprasPorMes);
 
 
 
-  //BEST SELLING
+
+  //----------------bestSelling----------------
   const productUnits = salesArray.reduce((acc, sale) => {
     if (acc[sale.productId]) {
       acc[sale.productId] += sale.units;
@@ -76,7 +67,9 @@ const comprasPorMesArray = Object.values(comprasPorMes);
   
 
 
+  //----------------offers----------------
   const offers = await Offer.find({sellerMail: mail});
+
 
   
   res.json({
@@ -94,11 +87,10 @@ const comprasPorMesArray = Object.values(comprasPorMes);
 
 
 
-
-
 const getProductData = async (productId) => {
   return await Product.findById(productId);
 }
+
 
 const getTotalSalesProducts = async (salesArray, productId) => {
 
@@ -115,23 +107,17 @@ const getTotalSalesProducts = async (salesArray, productId) => {
 }
 
 
-
-
 const updateSaleStock = async (id, stock) => {
-  // Find sale by ID
   const sale = await Product.findById(id);
 
   if (sale) {
-    // Update stock and save changes
     sale.stock -= stock;
     await sale.save();
   }
 };
 
 const getSeller = async (id) => {
-  // Find sale by ID
   const sale = await Product.findById(id);
-
   if (sale) {
     return sale.user;
   }
@@ -172,52 +158,8 @@ const salesPost = async(req, res = response) => {
       msg: "Stock negativo",
       sale
     })
-
   }
-
 }
-
-// const productsPut = async(req, res = response) => {
-
-//   const { id } = req.params
-
-//   const { _id, state, ...rest } = req.body //exclude status and _id & modify the ...rest data
-
-//   const productDB = await Product.findByIdAndUpdate(id, rest) //update the data (...rest) that have the same id
-
-//   res.json({
-//     msg: "get put - controller",
-//     productDB
-//   })
-// }
-
-// const productsDelete = async(req, res = response) => {
-
-//   const { id } = req.params
-
-//   const product = await Product.findByIdAndDelete(id)
-
-//   res.json({
-//     product
-//   })
-// }
-
-// const productExists = async(req, res = response) => {
-
-//   const { id } = req.params
-  
-//   // Verify if product exists
-//   const product = await Product.findById( id )
-//   if(!product){
-//     return res.status(200).json({
-//       msg: false
-//     })
-//   }
-
-//   res.json({
-//     msg: true
-//   })
-// }
 
 module.exports = {
   salesGet,
